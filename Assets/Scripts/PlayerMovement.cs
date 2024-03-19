3128bebf00a2d3ffba0fd99bdcdae65485c98d0c
kid-of-yesterday <kurlusha2@yandex.ru>
2024-03-19 15:13:28 +0100

    adding the mirror sync
    there is a mistake where two players spawn for some reason. trying to figure it out

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : NetworkBehaviour
+{
+   void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
+            transform.position += movement;
+
+        }
+    }
+
+    void Update()
+    {
+        HandleMovement();
+    }
+}
